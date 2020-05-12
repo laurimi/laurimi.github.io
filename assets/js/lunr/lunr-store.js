9d6ab14db7bdb97b207ca9e856f2ccafd6d31274
Mikko Lauri <lauri@informatik.uni-hamburg.de>
2020-05-12 10:56:52 +0200

    update pages

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Website started",
+        "excerpt":"Today, the first version of the website is up. I used jekyll, along with the minimal-mistakes theme and jekyll-scholar plugin for citations.  ","categories": ["news"],
+        "tags": [],
+        "url": "http://localhost:4000/news/2020/05/10/welcome.html",
+        "teaser": null
+      }]
