2e1e09bf78b393c7a76759e8bceb9e233ff52306
Mikko Lauri <lauri@informatik.uni-hamburg.de>
2020-05-30 10:04:26 +0200

    Deploy updates

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,10 +4,4 @@ var store = [{
         "tags": [],
         "url": "https://laurimi.github.io/news/2020/05/10/welcome.html",
         "teaser": null
-      },{
-        "title": "Active perception and decentralized decision-making",
-        "excerpt":"A lot of my research work deals with active perception. Active perception is a process by which an agent, a software agent or an embodied agent such as a robot, can perceive its environment to reduce uncertainty in order to accomplish some task. For instance, a robot that wants to...","categories": ["research"],
-        "tags": [],
-        "url": "https://laurimi.github.io/research/2020/05/18/active-perception.html",
-        "teaser": null
       }]
