2f7c8f5225eaf1346e2c4bbf5c4a3da895de7997
Mikko Lauri <lauri@informatik.uni-hamburg.de>
2020-07-07 08:37:06 +0200

    Deploy updates

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "https://laurimi.github.io/research/2020/06/13/active-perception.html",
         "teaser": null
+      },{
+        "title": "Multi-robot next-best-view planning",
+        "excerpt":"In this post, I give an overview of my recent paper (Lauri et al., 2020) on multi-robot next-best-view planning. Robots need three-dimensional scene models for many tasks ranging from manipulation to mapping and motion planning. To build a scene model, the robot needs to collect data, for example depth images...","categories": ["research"],
+        "tags": [],
+        "url": "https://laurimi.github.io/research/2020/07/04/multirobot-nbv.html",
+        "teaser": null
       }]
